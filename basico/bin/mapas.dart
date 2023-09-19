24043b17f41804007aab839dd9d9d19cc1499955
eduardobandeiramg <eduardobandeiramg@gmail.com>
2023-09-19 09:13:13 -0300

    adicionando mapas

---
+++ basico/bin/mapas.dart
@@ -0,0 +1,16 @@
+class maps{
+  //Nao permitem repetição de chaves
+
+  Map<String , int> mapa = {}; //declarando mapa
+  Map <String , int > outroMapa = { //declarando e inicializando mapa
+    "primeiro" : 1 ,
+    "segundo" : 2
+  };
+
+  mapa ["primeiro"] = 1; //adicionando valores a um mapa existente
+
+
+
+
+
+}
